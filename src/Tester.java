d38f5828d619fe5ccb796b2a0c09c86660c0ca88
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2023-02-22 14:43:26 -0800

    Update to remove imports

--- src/Tester.java
+++ src/Tester.java
@@ -1,30 +1,13 @@
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import me.neoblade298.neocore.shared.util.PaginatedList;
 
 public class Tester {
+
 	public static void main(String args[]) {
-		Calendar start = Calendar.getInstance();
-		Calendar newyear = Calendar.getInstance();
-		start.set(Calendar.DAY_OF_MONTH, 31);
-		start.set(Calendar.HOUR, 9);
-		newyear.set(Calendar.YEAR, 2023);
-		newyear.set(Calendar.MONTH, 1);
-		newyear.set(Calendar.DAY_OF_MONTH, 1);
-		newyear.set(Calendar.MINUTE, 0);
-		newyear.set(Calendar.HOUR, 0);
-		
-		System.out.println(start.getTime());
-		System.out.println(newyear.getTime());
-		System.out.println(getDateKey(start));
-		System.out.println(getDateKey(newyear));
-		System.out.println(getDateKey(newyear) - getDateKey(start));
-		
-		System.out.println((getDateKey(newyear) % 10000) / 100);
-		System.out.println(getDateKey(newyear) % 10);
-	}
-	
-	private static int getDateKey(Calendar c) {
-		return (c.get(Calendar.YEAR) * 10000) + ((c.get(Calendar.MONTH) + 1) * 100) + c.get(Calendar.DAY_OF_MONTH);
+		PaginatedList<Integer> list = new PaginatedList<Integer>();
+		for (int i = 0; i < 100; i++) {
+			list.add(i);
+		}
+		System.out.println(list.remove((i) -> {return 99 - i;}));
+		System.out.println(list.get(9));
 	}
 }
